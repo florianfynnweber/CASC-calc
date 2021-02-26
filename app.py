764bbe8815fd67f2633bfca18ab4be8fb4abcc31
Florian Weber <florian831@schule.bremen.de>
2021-02-26 15:56:08 +0100

    add file

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import logging
+import csv
+import json
+import argparse
+import tkinter
+
+# main minimal gui tkinter explrer stuff
+
+def report_csv(data):
+    pass
+
+def read_file(name):
+    pass
+
+def read_directoy(path):
+    pass
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
