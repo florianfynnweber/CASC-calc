61cd83d3e19aadc24112734e85741c16e03a718f
Florian Weber <florian831@schule.bremen.de>
2021-02-26 18:28:20 +0100

    ignore xlsx update app.py

--- app.py
+++ app.py
@@ -40,33 +40,37 @@ def report_csv(data):
 
 
 def read_file(name):
-    file_name = re.search("([a-zA-Z])+?_([0-9])+", os.path.split(name)[1]).group(0).split("_")
+    file_name = re.search(r"([a-zA-Z])+?_([0-9])+", os.path.split(name)[1]).group(0).split("_")
     dict_ = {
         "table":
             {
                 "x": [],
-                "y": []
+                "y": [],
+                "y'": []
             },
         "peak_table": {
-            "area": "",
-            "height": "",
-            "a/h": ""
+            "area": 0,
+            "height": 0,
+            "a/h": 0
         },
         "name": file_name[0],
-        "temp": file_name[1]
+        "temp": file_name[1],
+        "y_max": 0
     }
     with open(name, "r") as file:
         data = file.read()
-        for elm in re.finditer("[0-9],([0-9]){0,5}\t([\-]){0,1}([0-9])+?\n", data):
+        for elm in re.finditer(r"[0-9],([0-9]){0,5}\t([\-]){0,1}([0-9])+?\n", data):
             line = re.split(r'\t', elm.group(0))
             dict_["table"]["x"].append(format(float(line[0].rstrip().replace(",", ".")), '.5f'))
             dict_["table"]["y"].append(int(line[1].rstrip()))
-        other_data = re.search("2\t([^a-z])+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t", data).group(0).split("\t")
+            if dict_["y_max"] < int(line[1].rstrip()):
+                dict_["y_max"] = int(line[1].rstrip())
+        other_data = re.search(r"2\t([^a-z])+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t(.)+?\t", data).group(0).split("\t")
         print(f"area:{other_data[4]},heihg:{other_data[5]},a/h:{other_data[6]}")
         try:
-            dict_["peak_table"]["area"] =format(float(other_data[4].replace(",",".")),'.5f')
-            dict_["peak_table"]["height"] = format(float(other_data[5].replace(",",".")),'.5f')
-            dict_["peak_table"]["a/h"] = format(float(other_data[6].replace(",",".")),'.5f')
+            dict_["peak_table"]["area"] = float(format(float(other_data[4].replace(",", ".")), '.5f'))
+            dict_["peak_table"]["height"] = float(format(float(other_data[5].replace(",", ".")), '.5f'))
+            dict_["peak_table"]["a/h"] = float(format(float(other_data[6].replace(",", ".")), '.5f'))
         except ValueError:
             logging.error(file_name)
     return dict_
